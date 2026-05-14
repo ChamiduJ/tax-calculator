f2fa3a7d039797c6f91f5145564f976fdef78899
Chamidu Jayaruwan <chamidu@ccnetstar.com>
2026-05-14 22:16:58 +1200

    Code cleanup

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -2,8 +2,8 @@ import { render, screen } from '@testing-library/react'
 import App from '../App'
 
 describe('App', () => {
-  it('renders the get started heading', () => {
+  it('renders the tax calculator heading', () => {
     render(<App />)
-    expect(screen.getByRole('heading', { name: /get started/i })).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /tax calculator/i })).toBeInTheDocument()
   })
 })
