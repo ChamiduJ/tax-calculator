47aadfc48c19db99535faea3bb6894026e447166
Chamidu Jayaruwan <chamidu@ccnetstar.com>
2026-05-14 22:49:03 +1200

    Add CalculatorTaxes component

--- src/Components/Calculator/Calculator.tsx
+++ src/Components/Calculator/Calculator.tsx
@@ -1,10 +1,20 @@
 import CalculatorForm from './CalculatorForm/CalculatorForm'
 import CalculatorTaxes from './CalculatorTaxes/CalculatorTaxes'
 
+const mockTaxData = {
+  bands: [
+    { bandStart: 0, bandFinish: 14000, taxRate: 11.5, taxCollected: 1610.0 },
+    { bandStart: 14000, bandFinish: 48000, taxRate: 21.0, taxCollected: 7140.0 },
+    { bandStart: 48000, bandFinish: 70000, taxRate: 31.5, taxCollected: 6930.0 },
+    { bandStart: 70000, bandFinish: null, taxRate: 35.5, taxCollected: 10650.0 },
+  ],
+  total: 26330.0,
+}
+
 const Calculator = () => (
   <div>
     <CalculatorForm onSubmit={() => {}} />
-    <CalculatorTaxes />
+    <CalculatorTaxes taxData={mockTaxData} />
   </div>
 )
 
--- src/Components/Calculator/CalculatorTaxes/CalculatorTaxes.tsx
+++ src/Components/Calculator/CalculatorTaxes/CalculatorTaxes.tsx
@@ -1,3 +1,49 @@
-const CalculatorTaxes = () => <div>CalculatorTaxes</div>
+import type { FC } from 'react'
+import s from './styles.module.scss'
+
+type TaxBand = {
+  bandStart: number
+  bandFinish: number | null
+  taxRate: number
+  taxCollected: number
+}
+
+type CalculatorTaxesProps = {
+  taxData: {
+    bands: TaxBand[]
+    total: number
+  }
+}
+
+const CalculatorTaxes: FC<CalculatorTaxesProps> = ({ taxData: { bands, total } }) => (
+  <table className={s.calculatorTaxes}>
+    <thead>
+      <tr>
+        <th>Band Start</th>
+        <th>Band Finish</th>
+        <th>Tax Rate</th>
+        <th>Tax Collected</th>
+      </tr>
+    </thead>
+    <tbody>
+      {bands.map((band, index) => (
+        <tr key={index}>
+          <td>{band.bandStart}</td>
+          <td>{band.bandFinish}</td>
+          <td>{band.taxRate}%</td>
+          <td>{band.taxCollected}</td>
+        </tr>
+      ))}
+      <tr>
+        <td colSpan={3}>
+          <b>Total</b>
+        </td>
+        <td>
+          <b>{total}</b>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+)
 
 export default CalculatorTaxes
